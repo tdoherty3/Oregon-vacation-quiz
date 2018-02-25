dae454385129d2b096b9f69b7874ec09a170edaf
Thomas Doherty <thomas@selfsustain.com>
2018-02-24 20:23:50 -0800

    Trouble Shoot jquery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,26 @@
 $(document).ready(function() {
-  $("form#vacation").submit(function() {
+  $("form#vacation").submit(function(event) {
     event.preventDefault();
 
       var firstname = $ ("input#firstname").val();
+      console.log(firstname);
       var lastname = $ ("input#lastname").val();
-      var distanceinput = parseInt($("#distanceinput").val());
-      var temp = $ ("input#temp").val();
-      var risk = $ ("input#risk").val();
+      console.log(lastname);
+      var distance = parseInt($("#distanceinput").val());
+      console.log(distance);
+      var temp = $ ("#temp").val();
+      console.log(temp);
+      var risk = $ ("#risk").val();
+      console.log(risk);
       var drink = $ ("input:radio[name=drink]:checked").val();
+      console.log(drink);
 
       var result;
-      if (distance <= "20" && temp === "Warm" && risk === "Low"){
+      if (distance <= "20"){
       result = $('#result1').show();
-    //} else if (side1 === side2 || side2 === side3 || side1 === side3){
-      //result = $('#Isoceles').show();
+    } else if (distance <= "100"){
+      result = $('#result2').show();
+    }
     //} else if (side1 != side2 && side2 != side3 && side1 != side3){
       //result =$('#Scalene').show();
     //} else if (side1 === side2 && side2 === side3 && side1 === side3){
