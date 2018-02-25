007b1d97fd0a8002b03750801f2695e0455f11eb
Thomas Doherty <thomas@selfsustain.com>
2018-02-24 16:57:21 -0800

    Trouble Shoot jquery.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
 $(document).ready(function() {
-
-
+  $("form#vacation").submit(function() {
     event.preventDefault();
+
+      var firstname = $ ("input#firstname").val();
+      var lastname = $ ("input#lastname").val();
+      var distanceinput = parseInt($("#distanceinput").val());
+      var temp = $ ("input#temp").val();
+      var risk = $ ("input#risk").val();
+      var drink = $ ("input:radio[name=drink]:checked").val();
+
+      var result;
+      if (distance <= "20" && temp === "Warm" && risk === "Low"){
+      result = $('#result1').show();
+    //} else if (side1 === side2 || side2 === side3 || side1 === side3){
+      //result = $('#Isoceles').show();
+    //} else if (side1 != side2 && side2 != side3 && side1 != side3){
+      //result =$('#Scalene').show();
+    //} else if (side1 === side2 && side2 === side3 && side1 === side3){
+      //result = $('#Equilateral').show();
+      //}
+      $("#result").text(result);
+    });
   });
-});
